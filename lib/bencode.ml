e0a5c8ab768db06e5c9dc3aaf82ad842a2fb581e
Sam Mercer <smercer10@proton.me>
2024-06-27 17:18:11 +0100

    Avoid using l as variable name

--- lib/bencode.ml
+++ lib/bencode.ml
@@ -4,9 +4,9 @@ let decode_string_to_json s =
   match String.lsplit2 s ~on:':' with
   | Some (len, data) ->
     (match Int.of_string_opt len with
-     | Some l ->
-       if String.length data >= l
-       then Ok (`String (String.prefix data l))
+     | Some exp_len ->
+       if String.length data >= exp_len
+       then Ok (`String (String.prefix data exp_len))
        else Error "Data is too short"
      | None -> Error "Length is invalid")
   | None -> Error "String format is invalid"
