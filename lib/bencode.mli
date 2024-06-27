749a2fe73853a52bf417c1bb9bedcc89bdb91c67
Sam Mercer <smercer10@proton.me>
2024-06-27 17:37:20 +0100

    Clarify documentation for string decoder

--- lib/bencode.mli
+++ lib/bencode.mli
@@ -1,2 +1,3 @@
-(** [decode_string_to_json s] decodes a bencoded string [s] to JSON representation. *)
+(** [decode_string_to_json s] decodes a valid bencoded string [s] to JSON representation.
+    Returns [Error] if the bencoded string is invalid. *)
 val decode_string_to_json : string -> ([> `String of string ], string) Result.t
