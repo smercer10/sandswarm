d1fad3b6f183eb2881e5031c4b573d2d2c36bb2d
Sam Mercer <smercer10@proton.me>
2024-06-27 11:31:12 +0100

    Fix confusing variable name in string decoder

--- lib/bencode.ml
+++ lib/bencode.ml
@@ -4,9 +4,9 @@ let decode_string_to_json s =
   match String.lsplit2 s ~on:':' with
   | Some (len, data) ->
     (match Int.of_string_opt len with
-     | Some int ->
-       if String.length data >= int
-       then Ok (`String (String.prefix data int))
+     | Some l ->
+       if String.length data >= l
+       then Ok (`String (String.prefix data l))
        else Error "Data is too short"
      | None -> Error "Length is invalid")
   | None -> Error "String format is invalid"
