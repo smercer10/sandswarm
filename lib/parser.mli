d077a132f05bbe002b2bee0c17e1b28fcb1a9104
Sam Mercer <smercer10@proton.me>
2024-06-29 22:04:55 +0100

    Correct parser documentation

--- lib/parser.mli
+++ lib/parser.mli
@@ -2,5 +2,5 @@ open! Base
 
 (** [bval] is an [Angstrom] parser for bencoded values. For valid inputs, a JSON
     representation of the value is returned. If the input has invalid encoding, the
-    parser will raise an exception. *)
+    parser will fail. *)
 val bval : Yojson.Basic.t Angstrom.t
